95cd853ce0d98f9c0178bd7a3d530121793a7bab
Robert <anutarobertmarian@gmail.com>
2023-05-31 06:12:54 +0100

    Add event listener to the Add to cart button

--- front/js/product.script.js
+++ front/js/product.script.js
@@ -21,3 +21,60 @@ document.getElementById('colors').innerHTML = selectedProduct.colors.map((item) 
 
 //Add dynamically price
 document.getElementById('price').textContent = selectedProduct.price;
+
+//Get reference to my elements
+const addToCartButton = document.getElementById('addToCart');
+
+// Add event listener to "add to cart" button
+addToCartButton.addEventListener('click', () => {
+    //Get reference to my elements
+    const color = document.getElementById('colors').value;
+    const quantity = document.getElementById('quantity').value;
+    const image = document.getElementById('item_img').value;
+    // Get the product title and price 
+    const productName = document.getElementById('title').innerText;
+    const productPrice = document.getElementById('price').innerText;
+
+    // Check if the cart already contains the same product with the same color
+    const existingCartItem = getCartItem(productName, color);
+
+});
+
+
+// // Store the to add to cart product details in local storage and convert to js
+// localStorage.setItem('addToCartselectedProduct', JSON.stringify());
+// if (existingCartItem) {
+
+//     // If the product exists, update the quantity
+//     existingCartItem.quantity += quantity;
+// } else {
+//     // If the item doesn't exist, create a new cart item
+//     const cartItem = {
+//         productName,
+//         color,
+//         price: productPrice,
+//         quantity
+//     };
+
+//     // Add the new product to the cart
+//     const cartItems = getCartItems();
+//     cartItems.push(cartItem);
+//     saveCartItems(cartItems);
+
+//     //Redirect to the cart page
+//     document.location.href = 'http://127.0.0.1:5500/front/html/cart.html';
+
+//     // Function to save the cart items to local storage
+//     function saveCartItems(cartItems) {
+//         localStorage.setItem('cartItems', JSON.stringify(cartItems));
+//     }
+
+//     // Function to get a specific cart item by product name and color
+//     function getCartItem(productName, color) {
+//         const cartItems = getCartItems();
+//         return cartItems.find(item => item.productName === productName && item.color === color);
+//     }
+
+//     console.log(saveCartItems);
+//     console.log(getCartItem);
+// }
